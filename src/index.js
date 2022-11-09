1a21a93e5a6276b38af982853cb1a4a12f5fba4c
atefeh-karimi <atefehkarimikia0922@gmail.com>
2022-11-09 16:05:56 +0330

    scrollToTop

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
-
 import App from "./app";
+import ScrollToTop from "./component/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
+    <ScrollToTop />
     <App />
   </HashRouter>
 );
